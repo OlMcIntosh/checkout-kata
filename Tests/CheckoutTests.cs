dbfaf578b4877c53da892cc86420f9b6ff136b52
Olga <ol.mcintosh@gmail.com>
2023-08-17 21:06:04 +0100

    refactor tests

--- Tests/CheckoutTests.cs
+++ Tests/CheckoutTests.cs
@@ -4,15 +4,25 @@ namespace Tests
 {
     public class Tests
     {
+        Checkout checkout;
+
         [SetUp]
         public void Setup()
         {
+            checkout = new Checkout();
+        }
+
+        private static void ScanProducts(Checkout checkout, string productSKUs)
+        {
+            foreach (char code in productSKUs)
+            {
+                checkout.Scan(code.ToString());
+            }
         }
 
         [Test]
         public void when_one_item_is_scanned_the_total_price_equals_price_of_this_item()
-        {
-            var checkout = new Checkout();
+        {            
             checkout.SetPrice("A", 50);
 
             ScanProducts(checkout, "A");
@@ -23,7 +33,6 @@ namespace Tests
         [Test]
         public void when_two_of_the_same_item_is_scanned_and_no_special_prices_the_total_price_is_twice_the_price_of_item()
         {
-            var checkout = new Checkout();
             checkout.SetPrice("A", 50);
 
             ScanProducts(checkout, "AA");
@@ -46,7 +55,6 @@ namespace Tests
         [Test]
         public void when_special_price_is_set_for_two_items_and_two_items_scanned_the_total_is_special_price()
         {
-            var checkout = new Checkout();
             checkout.SetPrice("B", 30);
             checkout.SetSpecialPrice("B", 2, 45);
 
@@ -58,21 +66,12 @@ namespace Tests
         [Test]
         public void when_special_price_is_set_for_two_items_and_three_items_scanned_the_total_is_special_price_plus_single_item_price()
         {
-            var checkout = new Checkout();
             checkout.SetPrice("B", 30);
             checkout.SetSpecialPrice("B", 2, 45);
 
             ScanProducts(checkout, "BBB");
 
             Assert.That(checkout.GetTotalPrice(), Is.EqualTo(75));
-        }
-
-        private static void ScanProducts(Checkout checkout, string productSKUs)
-        {
-            foreach (char code in productSKUs) 
-            {
-                checkout.Scan(code.ToString());
-            }
         }
     }
 }
