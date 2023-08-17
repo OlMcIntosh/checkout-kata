b0e842ca65cfaef6a73d2ba3f4ade748bee4e088
Olga <ol.mcintosh@gmail.com>
2023-08-17 20:29:44 +0100

    -

--- CheckoutLibrary/Checkout.cs
+++ CheckoutLibrary/Checkout.cs
@@ -2,21 +2,40 @@
 {
     public class Checkout
     {
+        private Dictionary<string, int> prices = new Dictionary<string, int>();
+        private Dictionary<string, int> products = new Dictionary<string, int>();
+
         public Checkout()
         {
         }
 
         public int GetTotalPrice()
         {
-            return 50;
+            int total = 0;
+
+            foreach (var item in products)
+            {
+                total += prices[item.Key] * item.Value;
+            }
+
+            return total;
         }
 
-        public void Scan(string v)
+        public void Scan(string SKU)
         {
+            if (products.ContainsKey(SKU))
+            {
+                products[SKU]++;
+            }
+            else
+            {
+                products.Add(SKU, 1);
+            }
         }
 
-        public void SetPrice(string v1, int v2)
+        public void SetPrice(string SKU, int price)
         {
+            prices.Add(SKU, price);
         }
     }
 }
--- Tests/CheckoutTests.cs
+++ Tests/CheckoutTests.cs
@@ -10,7 +10,7 @@ namespace Tests
         }
 
         [Test]
-        public void when_one_item_is_scanned_the_total_price_iquals_price_of_this_item()
+        public void when_one_item_is_scanned_the_total_price_equals_price_of_this_item()
         {
             var checkout = new Checkout();
             checkout.SetPrice("A", 50);
@@ -18,6 +18,18 @@ namespace Tests
             checkout.Scan("A");
 
             Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void when_two_of_the_same_item_is_scanned_and_no_special_prices_the_total_price_is_twice_the_price_of_item()
+        {
+            var checkout = new Checkout();
+            checkout.SetPrice("A", 50);
+
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(100));
         }
     }
 }
